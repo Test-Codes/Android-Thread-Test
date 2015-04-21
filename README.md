4657f5159b3970da71a48befb289adb3ffc13bd6
TheFinestArtist <contact@thefinestartist.com>
2015-04-21 16:45:48 +0900

    Update Layout & themes & build skd version

--- README.md
+++ README.md
@@ -13,18 +13,18 @@ The task is to create a simple app with the requirements described below.
 
 ### App description
 
-We would like a single page app with 3 elements: 2 buttons and a text counter. The counter should be centered in the middle of the screen while the two buttons are placed at the bottom next to each other.
+(done) We would like a single page app with 3 elements: 2 buttons and a text counter. The counter should be centered in the middle of the screen while the two buttons are placed at the bottom next to each other.
 
 
 ### Requirements
 
-- The text counter should be placed in the middle of the screen. It should start at 0.
+- (done) The text counter should be placed in the middle of the screen. It should start at 0.
 - When clicking the first button, the counter should increase by 1.
 - When clicking the second button, it should start a background thread that updates the counter by 1 every second. Clicking the button again, should stop this background thread.
 - It should be possible to rotate the screen between portrait and landscape.
 - The app should stop incrementing the counter while being in the background, but it must remember its state if resumed, even if it has been killed.
-- TargetSDK is 22, MinSDK is 15.
-- The app should have a Material look and feel on Lollipop (SDK 21-22) and a Holo look below (SDK 14 - 19).
+- (done) TargetSDK is 22, MinSDK is 15.
+- (done) The app should have a Material look and feel on Lollipop (SDK 21-22) and a Holo look below (SDK 14 - 19).
 
 
 ## Evaluation
@@ -32,17 +32,21 @@ We would like a single page app with 3 elements: 2 buttons and a text counter. T
 The solution will be evaluated by the following criteria (in prioritized order):
 
 1) Is the solution thread safe? Please document why.
+
 2) Does it respect the Android lifecycle?
+
 3) Does it work across all supported SDK versions
+
 4) Are the code base and XML resources well structured?
+
 5) Are there tests?
+
 6) Does it follow Google design guidelines?
 
 3rd party libraries are allowed, but you must be prepared to defend why they are included and what problem they solve.
 
 
 ## More
-- Update Target SDK version to 22
 - Volatile, Translucent(?)
 - Atomic
 - final for variable, method, class
--- app/src/main/java/com/thefinestartist/realmandroidtest/MainActivity.java
+++ app/src/main/java/com/thefinestartist/realmandroidtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.thefinestartist.realmandroidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -9,7 +11,7 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @InjectView(R.id.counter_tv)
     TextView counterTv;
@@ -18,10 +20,79 @@ public class MainActivity extends Activity {
     @InjectView(R.id.second_bt)
     Button secondBt;
 
+    Thread counterThread;
+    boolean isThreadRunning = false;
+    int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        counterTv.setText(String.valueOf(count));
+        firstBt.setOnClickListener(this);
+        secondBt.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isThreadRunning)
+            toggleThread();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isThreadRunning)
+            toggleThread();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // When clicking the first button, the counter should increase by 1.
+            case R.id.first_bt:
+                updateCouter();
+                break;
+            // When clicking the second button, it should start a background thread that updates the counter by 1 every second. Clicking the button again, should stop this background thread.
+            case R.id.second_bt:
+                toggleThread();
+                break;
+        }
+    }
+
+    /**
+     * Private Methods
+     */
+    private void updateCouter() {
+        count++;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                counterTv.setText(String.valueOf(count));
+            }
+        });
+    }
+
+    private void toggleThread() {
+        if (counterThread == null) {
+            counterThread = new Thread() {
+                @Override
+                public void run() {
+                    while (true) {
+                        updateCouter();
+                        SystemClock.sleep(1000);
+                    }
+                }
+            };
+            counterThread.start();
+            isThreadRunning = true;
+        } else {
+            counterThread.interrupt();
+            counterThread = null;
+            isThreadRunning = false;
+        }
     }
 }
